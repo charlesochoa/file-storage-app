e766753746bfa28038e3d556525bf068bc893190
Gabriel Alvarez <gabrielaar11@gmail.com>
2020-05-25 17:22:12 +0200

    Add project files

---
+++ src/main/java/chatdist/filestorageapp/api/FileController.java
@@ -0,0 +1,71 @@
+package chatdist.filestorageapp.api;
+
+import chatdist.filestorageapp.service.FileStorageService;
+import chatdist.filestorageapp.util.UploadFileResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path="/files")
+public class FileController {
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    FileStorageService fileStorageService;
+
+    @PostMapping("/upload")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
+        if (file.getSize() < 20971520) {
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/files/download/")
+                    .path(fileName)
+                    .toUriString();
+
+            return new UploadFileResponse(fileName, fileDownloadUri,
+                    file.getContentType(), file.getSize());
+        }
+        throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "File size exceeds 20 MB!"
+        );
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = (Resource) fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""
+                        + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+}
